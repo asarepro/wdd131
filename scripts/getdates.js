0daff10e3de1b014d2b0932590d650ffd01d77bb
asarepro <langfordasare@gmail.com>
2026-03-07 06:08:05 +0000

    finished

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,4 +2,4 @@ const currentYear = new Date();
 const fullyear = currentYear.getFullYear();
 document.getElementById('lastModified').innerHTML = `Last Modification ${document.lastModified}`;
 
-const year = document.getElementById('year').innerHTML +=` ${fullyear} Asare Jeffrey | Ghana 😎`;
+const year = document.getElementById('year').innerHTML += ` ${fullyear} Asare Jeffrey | Ghana 😎`;
